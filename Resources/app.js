013fe0a91536482bfee05ae3c9219849b2c1c3c9
Kevin Mannix <kmannix@bu.edu>
2014-10-25 00:51:11 -0400

    Switching to iOS for time being

---
+++ Resources/app.js
@@ -0,0 +1,47 @@
+var Map = require('ti.map');
+var win = Titanium.UI.createWindow();
+
+if (Ti.Geolocation.locationServicesEnabled) {
+    Ti.Geolocation.purpose = 'Get Current Location';
+    Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
+    Ti.Geolocation.distanceFilter = 10;
+    Ti.Geolocation.preferredProvider = Ti.Geolocation.PROVIDER_GPS;
+
+    Ti.Geolocation.addEventListener('location', function(e) {
+        if (e.error) {
+            alert('Error: ' + e.error);
+        } else {
+            Ti.API.info(e.coords);
+        }
+    });
+} else {
+    alert('Please enable location services');
+}
+
+var mountainView = Map.createAnnotation({
+    latitude:37.390749,
+    longitude:-122.081651,
+    title:"Appcelerator Headquarters",
+    subtitle:'Mountain View, CA',
+    pincolor:Map.ANNOTATION_RED,
+    myid:1 // Custom property to uniquely identify this annotation.
+});
+
+var mapview = Map.createView({
+    mapType: Map.NORMAL_TYPE,
+    region: {latitude:33.74511, longitude:-84.38993,
+            latitudeDelta:0.01, longitudeDelta:0.01},
+    animate:true,
+    regionFit:true,
+    userLocation:true,
+    annotations:[mountainView]
+});
+
+Ti.API.info("Checking in");
+
+win.add(mapview);
+// Handle click events on any annotations on this map.
+mapview.addEventListener('click', function(evt) {
+    Ti.API.info("Annotation Double click");
+});
+win.open();
